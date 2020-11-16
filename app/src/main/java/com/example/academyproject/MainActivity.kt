e7c06e27a6faec95a3373e54843330a8b77a010f
Pavel Dashkevich <pavel.dashkevich@gmail.com>
2020-11-16 21:09:29 +0300

    "Movie details" layout realized.

--- app/src/main/java/com/example/academyproject/MainActivity.kt
+++ app/src/main/java/com/example/academyproject/MainActivity.kt
@@ -1,38 +1,35 @@
 package com.example.academyproject
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.shape.CornerFamily
 
 class MainActivity : AppCompatActivity() {
 
+    private fun applyCornersToActorPhoto(rad: Float, img: ShapeableImageView?) {
+        img?.shapeAppearanceModel
+                ?.toBuilder()
+                ?.setTopRightCorner(CornerFamily.ROUNDED, rad)
+                ?.setTopLeftCorner(CornerFamily.ROUNDED, rad)
+                ?.setBottomRightCorner(CornerFamily.ROUNDED, rad)
+                ?.setBottomLeftCorner(CornerFamily.ROUNDED, rad)
+                ?.build()
+    }
+
+    private fun applyCornersToActorsPhotos() {
+        val rad: Float = resources.getDimension(R.dimen.actor_photo_corner_radius)
+
+        applyCornersToActorPhoto(rad, findViewById(R.id.siv_movie_actor_photo1))
+        applyCornersToActorPhoto(rad, findViewById(R.id.siv_movie_actor_photo2))
+        applyCornersToActorPhoto(rad, findViewById(R.id.siv_movie_actor_photo3))
+        applyCornersToActorPhoto(rad, findViewById(R.id.siv_movie_actor_photo4))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        setSupportActionBar(findViewById(R.id.toolbar))
+        setContentView(R.layout.activity_movie_details)
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
-        }
+        applyCornersToActorsPhotos()
     }
 }
